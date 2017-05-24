d09bee66bdaddb0e9f652c6326016be81cc3a36e
Tzaoh <tzaoh@users.noreply.github.com>
2017-05-24 04:55:23 +0200

    Fix profile dropdown button v2

--- themes/HugoMDL/static/js/fabbutton.js
+++ themes/HugoMDL/static/js/fabbutton.js
@@ -1,25 +1,29 @@
 (function () {
-	showOpts = function(e) {
-		
-		var processClick = function (evt) {
-			if (e !== evt) {
-				for (var i = 0; i < list_ctns.length; i++) {
-					if (list_ctns[i].IS_SHOWING) {
-						list_ctns[i].classList.remove(VISIBLE_CLASS);
-						list_ctns[i].IS_SHOWING = false;
-						document.removeEventListener("click", processClick);
-					}
-				}
-				
+	
+	closeRest = function(notElem) {
+		var ctn;
+		for (var i=0; i<list_ctns.length; i++) {
+			ctn = list_ctns[i];
+			if (ctn != notElem && ctn.IS_SHOWING) {
+				ctn.IS_SHOWING = false;
+				ctn.classList.remove(VISIBLE_CLASS);
 			}
-		};
-		
+		}
+	}
+	
+	showHideOpts = function(e) {
 		if (!this.IS_SHOWING) {
 			this.IS_SHOWING = true;
 			this.classList.add(VISIBLE_CLASS);
-			document.addEventListener("click", processClick);
+			closeRest(this);
+		} else {
+			this.IS_SHOWING = false;
+			this.classList.remove(VISIBLE_CLASS);
 		}
-	};
+		
+		e.preventDefault();
+		e.stopPropagation();
+	}
 	
 	var container_static_name = "fab_ctn";
 	var VISIBLE_CLASS = "is-showing-options";
@@ -27,11 +31,11 @@
 	
 	for (i=1; i<=2; i++) {
 		fab_ctn = document.getElementById(container_static_name + '_' + i);
-		
 		if (fab_ctn != null) {
 			list_ctns.push(fab_ctn)
-			fab_ctn.addEventListener("click", showOpts);
+			fab_ctn.addEventListener("click", showHideOpts);
 		}
 	}
 	
+	document.addEventListener("click", closeRest);
 }.call(this));
